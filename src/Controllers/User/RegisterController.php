6b18124d582ec7569a9a4ca4336b083d20f8f2b5
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2024-02-24 21:29:28 -0300

    Controllers fluxo de usuário

---
+++ src/Controllers/User/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Easy\Wallet\Controllers\User;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Easy\Wallet\Controllers\AbstractController;
+use Easy\Wallet\Services\UserService;
+
+class RegisterController extends AbstractController
+{
+    public function __construct(
+        protected readonly UserService $service
+    ) {
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new Response(200, []);
+    }
+}
