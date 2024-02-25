c7e5c6afc863c6205d17c0db223b81644e5579d1
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2024-02-25 11:47:03 -0300

    Criado função exception

--- src/Services/AbstractService.php
+++ src/Services/AbstractService.php
@@ -6,6 +6,14 @@ use Psr\Http\Message\ServerRequestInterface;
 
 abstract class AbstractService
 {
+    public static function exception(int $code, array $data): array
+    {
+        return [
+            'code' => 404,
+            'data' => $data
+        ];
+    }
+
     public function hydrateData(ServerRequestInterface $request): array
     {
         return json_decode($request->getBody()->getContents(), true);
