483c4f7026a22f877837f4289e74e435e0df3245
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2024-02-25 23:14:44 -0300

    Entity de usuário

---
+++ src/Domain/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Easy\Wallet\Domain\Entity;
+
+class User
+{
+    private int $id;
+
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password,
+        public readonly string $type,
+        public readonly string $active,
+        public readonly string $document,
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
