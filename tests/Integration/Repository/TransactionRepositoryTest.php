0f34c4560449becbea987b699fe4a09a1a16a471
wesley <wesley.vmartins.js@gmail.com>
2024-02-27 11:34:49 -0300

    Implementando teste de transações

--- tests/Integration/Repository/TransactionRepositoryTest.php
+++ tests/Integration/Repository/TransactionRepositoryTest.php
@@ -2,6 +2,12 @@
 
 namespace Integration\Repository;
 
+use Easy\Wallet\Domain\DTO\CreateTransferDTO;
+use Easy\Wallet\Domain\Entity\Transaction;
+use Easy\Wallet\Domain\Enum\TransactionSubtypeEnum;
+use Easy\Wallet\Domain\Enum\TransactionTypeEnum;
+use Easy\Wallet\Repositories\TransactionRepository;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class TransactionRepositoryTest extends TestCase
@@ -21,7 +27,7 @@ class TransactionRepositoryTest extends TestCase
                 status INTEGER NOT NULL,
                 created_at TEXT DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (user_id) REFERENCES users(id)
-            );
+            )
         ");
     }
 
@@ -30,9 +36,39 @@ class TransactionRepositoryTest extends TestCase
         self::$pdo->beginTransaction();
     }
 
-    public function testTransaction()
+    #[DataProvider('transferFrom')]
+    public function testCreationOfTransferAndItsCounterpart(CreateTransferDTO $transactions)
     {
-        $this->assertEquals(1, true);
+        $transaction = new TransactionRepository(self::$pdo);
+
+        $transferFrom = new Transaction(
+            TransactionTypeEnum::TRANSFER,
+            TransactionSubtypeEnum::EXPENSE,
+            $transactions->userFrom,
+            $transactions->value,
+            1
+        );
+
+        $transferTo  = new Transaction(
+            TransactionTypeEnum::TRANSFER,
+            TransactionSubtypeEnum::INCOME,
+            $transactions->userTo,
+            $transactions->value,
+            1
+        );
+
+        $resultTransferFrom = $transaction->register((array) $transferFrom);
+        $resultTransferTo = $transaction->register((array) $transferTo);
+
+        $this->assertTrue($resultTransferFrom);
+        $this->assertTrue($resultTransferTo);
+    }
+
+    public static function transferFrom(): array
+    {
+        return [
+            new CreateTransferDTO(1, 2, 10000)
+        ];
     }
 
     protected function tearDown(): void
